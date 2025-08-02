4411a2a0b6f2adfcf45f128adc581d01fb97cafa
davidramir <emilio.ramirez@uab.edu.bo>
2025-08-02 18:15:21 -0400

    Creacion de las tablas Marcas y productos RELACIONADAS

---
+++ backend/database/migrations/2025_08_02_220247_create_productos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre',80);
+            $table->decimal('precio',10,2);
+            $table->timestamps();
+
+            $table->unsignedBigInteger('marca_id')->nullable();
+
+            $table->foreign('marca_id')->references('id')->on('marcas')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('productos');
+    }
+};
