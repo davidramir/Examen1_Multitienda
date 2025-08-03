bf9a5c63fda9456c764b4713ddccffa5037caa3b
davidramir <emilio.ramirez@uab.edu.bo>
2025-08-03 11:10:02 -0400

    CRUD completo para la tabla Productos

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\http\Controllers\MarcaController;
+use App\Http\Controllers\ProductoController;
+use App\Http\Controllers\MarcaController;
 
+
+Route::apiResource('productos', ProductoController::Class);
 
 Route::apiResource('marcas', MarcaController::Class);
