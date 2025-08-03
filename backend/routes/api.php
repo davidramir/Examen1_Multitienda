4a1406df6b85f5743687999140cd98e8fe47dc8a
davidramir <emilio.ramirez@uab.edu.bo>
2025-08-03 14:11:06 -0400

    Creacion del controlador Auth

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,8 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductoController;
 use App\Http\Controllers\MarcaController;
+use App\Http\Controllers\AuthController;
 
 
 Route::apiResource('productos', ProductoController::Class);
 
 Route::apiResource('marcas', MarcaController::Class);
+
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::post('/login', [AuthController::class, 'login']);
